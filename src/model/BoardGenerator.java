7ff9528525026eb88a69f6ed04c79106ad666952
skh <kh.syerikjan@gmail.com>
2016-03-21 12:03:59 +1100

    Removed unused imports

--- src/model/BoardGenerator.java
+++ src/model/BoardGenerator.java
@@ -2,29 +2,25 @@ package model;
 
 public class BoardGenerator {
 
-	
 	public Object[][] generateStartBoard() {
-		
+
 		Object[][] boardData = new Object[GameConfig.getRowCol()][GameConfig.getRowCol()];
-		
-		
+
 		for (int i = 0; i < boardData.length; i++) {
 			for (int j = 0; j < boardData[i].length; j++) {
 				boardData[i][j] = null;// blank
-				
-				
-				if(i==2&&j==3){
+
+				if (i == 2 && j == 3) {
 					boardData[i][j] = 'x';
 				}
 
-				//System.out.println(".");
-				boardData[i][j] = '.';
+				// System.out.println(".");
+				boardData[i][j] = null;
 			}
 		}
-		
-		
+
 		return boardData;
-		
+
 	}
-	
+
 }
