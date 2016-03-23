9931c6a662d300052499150935b0d543375f8c16
skh <kh.syerikjan@gmail.com>
2016-03-23 20:02:37 +1100

    Formatted the comment

--- src/controller/TraitBuilder.java
+++ src/controller/TraitBuilder.java
@@ -6,52 +6,61 @@ import java.util.Random;
 import model.traits.*;
 
 public class TraitBuilder {
-	
+
 	private Random randomNumGen;
 	private TraitSet traitSet;
-	
-	/*Seeded Random generator passed into constructor
-	 * Trait set is created on instantiation of Trait Builder*/
-	
-	public TraitBuilder(Random randomNumGen){
-		
+
+	/**
+	 * Trait set is created on instantiation of Trait Builder
+	 * 
+	 * @param randomNumGen
+	 *            - Seeded Random generator
+	 */
+
+	public TraitBuilder(Random randomNumGen) {
+
 		this.randomNumGen = randomNumGen;
 		generateTraitSetfromTraits();
 	}
-	
-	/*All traits instantiated here, references passed to allocateTraitValues
-	 *  then aggregated into a trait set*/
-	
-	private void generateTraitSetfromTraits(){
-		
+
+	/**
+	 * All traits instantiated here, references passed to allocateTraitValues
+	 * then aggregated into a trait set
+	 */
+
+	private void generateTraitSetfromTraits() {
+
 		HealthTrait healthTrait = new HealthTrait();
 		MoveTrait moveTrait = new MoveTrait();
 		AttackTrait attackTrait = new AttackTrait();
 		DamageTrait damageTrait = new DamageTrait();
-		
+
 		allocateTraitValues(healthTrait, damageTrait, moveTrait);
-		
-		traitSet = new TraitSet(healthTrait, moveTrait, attackTrait, damageTrait);	
+
+		traitSet = new TraitSet(healthTrait, moveTrait, attackTrait, damageTrait);
 	}
-	
-	
-	/*Trait references with randomizable values packaged into ArrayList, 
-	 * passed to traitRandomizer along with random number generate*/
-	
-	private void allocateTraitValues(HealthTrait healthTrait, DamageTrait damageTrait, MoveTrait moveTrait){
-		
+
+	/**
+	 * Trait references with randomizable values packaged into ArrayList, passed
+	 * to traitRandomizer along with random number generate
+	 * 
+	 * @param healthTrait
+	 * @param damageTrait
+	 * @param moveTrait
+	 */
+	private void allocateTraitValues(HealthTrait healthTrait, DamageTrait damageTrait, MoveTrait moveTrait) {
+
 		ArrayList<Trait> listOfTraits = new ArrayList<Trait>();
-		
+
 		listOfTraits.add(damageTrait);
 		listOfTraits.add(moveTrait);
 		listOfTraits.add(healthTrait);
-		
+
 		TraitRandomizer traitRandomizer = new TraitRandomizer();
 		traitRandomizer.generateRandomTraitValues(listOfTraits, randomNumGen);
 	}
-	
-	
-	public TraitSet getTraitSet(){
+
+	public TraitSet getTraitSet() {
 		return traitSet;
 	}
 }
