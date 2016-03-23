52d9260546afd13c01a1973f6760d1900e732feb
rmit-s3470484-daniel-stepanenko <s3470484@student.rmit.edu.au>
2016-03-23 14:54:36 +1100

    Completed prototype of trait value randomizer. TraitSet now ready for composition into Piece.

--- src/controller/TraitBuilder.java
+++ src/controller/TraitBuilder.java
@@ -10,11 +10,17 @@ public class TraitBuilder {
 	private Random randomNumGen;
 	private TraitSet traitSet;
 	
+	/*Seeded Random generator passed into constructor
+	 * Trait set is created on instantiation of Trait Builder*/
+	
 	public TraitBuilder(Random randomNumGen){
 		
 		this.randomNumGen = randomNumGen;
 		generateTraitSetfromTraits();
 	}
+	
+	/*All traits instantiated here, references passed to allocateTraitValues
+	 *  then aggregated into a trait set*/
 	
 	private void generateTraitSetfromTraits(){
 		
@@ -28,6 +34,10 @@ public class TraitBuilder {
 		traitSet = new TraitSet(healthTrait, moveTrait, attackTrait, damageTrait);	
 	}
 	
+	
+	/*Trait references with randomizable values packaged into ArrayList, 
+	 * passed to traitRandomizer along with random number generate*/
+	
 	private void allocateTraitValues(HealthTrait healthTrait, DamageTrait damageTrait, MoveTrait moveTrait){
 		
 		ArrayList<Trait> listOfTraits = new ArrayList<Trait>();
@@ -37,7 +47,7 @@ public class TraitBuilder {
 		listOfTraits.add(healthTrait);
 		
 		TraitRandomizer traitRandomizer = new TraitRandomizer();
-		traitRandomizer.generateRandomTraitValue(listOfTraits, randomNumGen);
+		traitRandomizer.generateRandomTraitValues(listOfTraits, randomNumGen);
 	}
 	
 	
--- src/controller/TraitRandomizer.java
+++ src/controller/TraitRandomizer.java
@@ -6,11 +6,18 @@ import java.util.Random;
 import model.traits.*;
 
 public class TraitRandomizer {
+	
+	final int VALUESDISTRIBUTED = 6;
+	final int VALUEINCREMENT = 1;
 
-	public int generateRandomTraitValue(ArrayList<Trait> listofTraits, Random randomNumGen){
+	public void generateRandomTraitValues(ArrayList<Trait> listofTraits, Random randomNumGen){
 		
-		//TODO
-		return 0;
+		int randomNum = randomNumGen.ints(0, (listofTraits.size() + 1)).limit(1).findFirst().getAsInt();
+		
+		for (int i = 0; i < VALUESDISTRIBUTED; i++) {
+			
+			listofTraits.get(randomNum).modifyValue(VALUEINCREMENT);
+		}
 	}
 	
 }
