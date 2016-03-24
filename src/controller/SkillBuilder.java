b439e63ecfcfc670eb5a58761c734e4d27251142
skh <kh.syerikjan@gmail.com>
2016-03-24 11:35:46 +1100

    Speed skill to range skill

--- src/controller/SkillBuilder.java
+++ src/controller/SkillBuilder.java
@@ -5,30 +5,27 @@ import java.util.Random;
 import model.skills.AttackSkill;
 import model.skills.BuildSkill;
 import model.skills.HealSkill;
+import model.skills.RangeSkill;
 import model.skills.SkillSet;
-import model.skills.SpeedSkill;
-import model.traits.TraitSet;
 
 public class SkillBuilder {
-	
+
 	private Random randomNumGen;
 	private SkillSet skillSet;
-	
-	public SkillBuilder(Random randomNumGen){
-	
-			this.randomNumGen = randomNumGen;
-			generateSkillSetFromSkills();
-		}
+
+	public SkillBuilder(Random randomNumGen) {
+
+		this.randomNumGen = randomNumGen;
+		generateSkillSetFromSkills();
+	}
 
 	private void generateSkillSetFromSkills() {
-		
+
 		HealSkill healSkill = new HealSkill();
 		BuildSkill buildSkill = new BuildSkill();
-		SpeedSkill speedSkill = new SpeedSkill();
+		RangeSkill speedSkill = new RangeSkill();
 		AttackSkill attackSkill = new AttackSkill();
-		
+
 	}
-	
-	
 
 }
