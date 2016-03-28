33bafeaa44b5e30262f8557e31f92e4a82ad5735
rmit-s3470484-daniel-stepanenko <s3470484@student.rmit.edu.au>
2016-03-28 23:17:16 +1100

    HeakSkill implemented with exception handling, requires testing

--- src/model/skills/HealSkill.java
+++ src/model/skills/HealSkill.java
@@ -1,28 +1,55 @@
 package model.skills;
 
+
 import model.Piece;
 import model.Square;
 
-// TODO: Implement the logic
 
 public class HealSkill extends Skill implements IPerformSquareSkill {
 
+	final int HEALAMOUNT = 1;
+	
 	public boolean performSkill(Square sqr, Piece skillOwner) {
 		
+		boolean result = false;
 		Piece targetPiece;
 		
-		if(sqr.getOccupant() != null){
-			
-			targetPiece = sqr.getOccupant();
-			
-			if (targetPiece.getTeam() == skillOwner.getTeam()){
-				
-				
+		try{
+			if(sqr.getOccupant() == null){
+				throw new IncorrectSquareException("No piece in square.");
 			}
-			
+			else{
+				targetPiece = sqr.getOccupant();
+				
+				if (targetPiece.getTeam() != skillOwner.getTeam()){
+					
+					throw new IncorrectSquareException("No piece in square.");
+				}
+				else{
+					targetPiece.getTraitSet().getHealthTrait().modifyValue(HEALAMOUNT);	
+					result = true;
+				}
+			}
 		}
-		
-		
+	catch(IncorrectSquareException e){
+		 System.out.println(e.getMessage());
+		}
+		return result;
 	}
+}
+
+
+class IncorrectSquareException extends RuntimeException { 
+	
+	public IncorrectSquareException() { 
+		super(); 
+	} 
+	
+	public IncorrectSquareException(String message) {
+		super(message);
+	}
+	
 
 }
+
+
