f0645679eadd0b345d340b567a551e9847983c07
Mark Scicluna <dicarkus@hotmail.com>
2016-04-06 19:41:37 +1000

    cleaned up code, removed unused classes

--- src/controller/PieceBuilder.java
+++ src/controller/PieceBuilder.java
@@ -23,7 +23,7 @@ public class PieceBuilder {
 		newPiece.setTraitSet(newTraitSet);
 		newPiece.setSkillSet(newSkillSet);
 		newPiece.setTeam(team);
-		
-		System.out.println("--------------- skillll!!! = " + newSkillSet.getCurrentSkill().toString());
+//		
+//		System.out.println("--------------- skillll!!! = " + newSkillSet.getCurrentSkill().toString());
 	}
 }
