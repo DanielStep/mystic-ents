892c9c52525b10661265745616ba6c5f7e8da630
rmit-s3470484-daniel-stepanenko <s3470484@student.rmit.edu.au>
2016-04-07 18:13:19 +1000

    HealthTrait javadoc

--- src/model/traits/HealthTrait.java
+++ src/model/traits/HealthTrait.java
@@ -1,5 +1,9 @@
 package model.traits;
-
+/**
+ * Class that extends Trait that represents a piece's health
+ * @author Daniel
+ *
+ */
 public class HealthTrait extends Trait {
 
 	public HealthTrait(int startingValue) {
