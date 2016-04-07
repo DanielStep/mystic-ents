68e9f6289d880f4042183a4255888c698e31d3ea
rmit-s3470484-daniel-stepanenko <s3470484@student.rmit.edu.au>
2016-04-07 18:17:38 +1000

    SkillRandomizer javadoc amended

--- src/model/skills/SkillRandomizer.java
+++ src/model/skills/SkillRandomizer.java
@@ -4,15 +4,9 @@ import java.util.Random;
 /**
  * Class is passed list of all skills and randomly assigns one to a skill set
  * @author Daniel
- *
+ * @modified, Mark
  */
 public class SkillRandomizer {
-	
-	/*public Skillset randomAllocationToSet(ArrayList<Skill> listOfSkills, Random randomNumGen){	
-		int randomNum = randomNumGen.ints(0, listOfSkills.size()).limit(1).findFirst().getAsInt();
-		Skillset skillset = new SkillSet(listOfSkills.get(randomNum));	
-		return skillset;
-	}*/
 	
 	/**
 	 * Method is passed list of all skills and randomly assigns one to a skill set which
@@ -22,6 +16,13 @@ public class SkillRandomizer {
 	 * @param randomNumGen
 	 * @return SkillSet
 	 */
+	/*public Skillset randomAllocationToSet(ArrayList<Skill> listOfSkills, Random randomNumGen){	
+		int randomNum = randomNumGen.ints(0, listOfSkills.size()).limit(1).findFirst().getAsInt();
+		Skillset skillset = new SkillSet(listOfSkills.get(randomNum));	
+		return skillset;
+	}*/
+	
+	
 	public Skill randomAllocationToSet(ArrayList<Skill> listOfSkills) {		
 		Random randomiser = new Random();
         int index = randomiser.nextInt(listOfSkills.size());
