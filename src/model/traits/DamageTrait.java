2063489ee1323634283782425652389df2441767
rmit-s3470484-daniel-stepanenko <s3470484@student.rmit.edu.au>
2016-04-07 18:12:39 +1000

    DamageTrait javadoc

--- src/model/traits/DamageTrait.java
+++ src/model/traits/DamageTrait.java
@@ -1,5 +1,9 @@
 package model.traits;
-
+/**
+ * Class that extends Trait and represents the damage a piece can do by attacking
+ * @author Daniel
+ *
+ */
 public class DamageTrait extends Trait {
 
 	public DamageTrait(int startingValue) {
