6386540f799e51b78f74826c952da58032050dc5
skh <kh.syerikjan@gmail.com>
2016-04-11 13:39:02 +1000

    Added comment for Modifier interface

--- src/model/skills/Modifier.java
+++ src/model/skills/Modifier.java
@@ -2,6 +2,13 @@ package model.skills;
 
 import model.Piece;
 
+/**
+ * Interface for modifying piece trait, skill values.
+ * 
+ * @author skh
+ *
+ */
+
 public interface Modifier {
 
 	public void applyModifier(Piece skillOwner);
