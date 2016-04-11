01e9797f258564da9c399b7e996166a8576d22c3
skh <kh.syerikjan@gmail.com>
2016-04-11 11:05:09 +1000

    Made PieceView label private

--- src/view/PieceView.java
+++ src/view/PieceView.java
@@ -20,7 +20,7 @@ import model.Piece;
  *
  */
 public class PieceView extends JPanel {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	// Represents team.
@@ -28,45 +28,45 @@ public class PieceView extends JPanel {
 	private Piece piece;
 	private GameUtils gameUtils;
 	private int size = 40;
-	JLabel label;
-	
+	private JLabel label;
+
 	PieceView(Piece pce) {
 		gameUtils = GameUtils.getInstance();
-		size = (int) ((GameConfig.getDefaultHeight() / GameConfig.getROW_COL())-3.5);
+		size = (int) ((GameConfig.getDefaultHeight() / GameConfig.getROW_COL()) - 3.5);
 		this.piece = pce;
 		this.color = gameUtils.stringToColor(pce.getTeam().name(), Color.WHITE);
 		addPieceLabel();
 	}
-	
-	private void addPieceLabel() {		
-	    label = new JLabel(buildLabelString(), JLabel.LEFT);
-	    label.setFont(new Font("Sans-serif", Font.PLAIN, (int) (size/2.5)));
-	    if (piece.getIsUsurper()){
-	    	label.setForeground(this.color);
-	    } else {
-	    	label.setForeground(Color.white);
-	    }
-	    this.add(label);
-	}	
-	
-	private String buildLabelString() {		
+
+	private void addPieceLabel() {
+		label = new JLabel(buildLabelString(), JLabel.LEFT);
+		label.setFont(new Font("Sans-serif", Font.PLAIN, (int) (size / 2.5)));
+		if (piece.getIsUsurper()) {
+			label.setForeground(this.color);
+		} else {
+			label.setForeground(Color.white);
+		}
+		this.add(label);
+	}
+
+	private String buildLabelString() {
 		StringBuilder str = new StringBuilder();
 		str.append(piece.getSkillSet().getCurrentSkill().getName().charAt(0));
 		return str.toString();
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		if (piece.getIsUsurper()){
+		if (piece.getIsUsurper()) {
 			g2.setPaint(Color.white);
 			g2.fillOval(0, 0, size, size);
 			g2.setPaint(this.color);
 			g2.setStroke(new BasicStroke(4));
-			g.drawOval(2, 2, size-4, size-4);			
+			g.drawOval(2, 2, size - 4, size - 4);
 		} else {
 			g2.setPaint(this.color);
-			g2.fillOval(0, 0, size, size);		
+			g2.fillOval(0, 0, size, size);
 		}
 	}
 
