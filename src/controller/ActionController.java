5088c2597a696c0a3bc3591bcdb3a64c3ea504c8
skh <kh.syerikjan@gmail.com>
2016-05-08 14:44:09 +1000

    Removed savetoMemento from ActionController

--- src/controller/ActionController.java
+++ src/controller/ActionController.java
@@ -102,9 +102,8 @@ public class ActionController {
 	
 	private void endTurn() {		
 		// automatically switch player when finishing a move
-		gameController.getGameTurn().setGameTimer(0);		
-		// saving game board for undo
-		boardController.saveToMemento();
+		gameController.getGameTurn().setGameTimer(0);
+
 		//reset actionCount;
 		actionCount = 0;
 	}
