a7e11499afd2e557ba404db1d38ecb29f05895a1
skh <kh.syerikjan@gmail.com>
2016-05-18 20:20:11 +1000

    Implemented flywight pattern in skills creation

--- src/controller/SkillFactory.java
+++ src/controller/SkillFactory.java
@@ -1,17 +1,17 @@
 package controller;
 
+import java.util.HashMap;
+
 import model.skills.AttackSkill;
 import model.skills.BuildSkill;
 import model.skills.HealSkill;
 import model.skills.RangeSkill;
 import model.skills.Skill;
-import model.traits.AttackTrait;
-import model.traits.DamageTrait;
-import model.traits.HealthTrait;
-import model.traits.RangeTrait;
 import model.traits.Trait;
 
 public class SkillFactory extends AbstractFactory {
+
+	private static HashMap<String, Skill> skills = new HashMap<>();
 
 	@Override
 	public Trait makeTrait(String traitType) {
@@ -21,21 +21,27 @@ public class SkillFactory extends AbstractFactory {
 	@Override
 	public Skill makeSkill(String skillType) {
 
+		Skill sk = skills.get(skillType);
+
 		if (skillType == null) {
 			return null;
 		}
 
-		if (skillType.equalsIgnoreCase("ATTACK")) {
-			return new AttackSkill();
-		} else if (skillType.equalsIgnoreCase("RANGE")) {
-			return new RangeSkill();
-		} else if (skillType.equalsIgnoreCase("BUILD")) {
-			return new BuildSkill();
-		} else if (skillType.equalsIgnoreCase("HEAL")) {
-			return new HealSkill();
+		if (sk == null) {
+
+			if (skillType.equalsIgnoreCase("ATTACK")) {
+				sk = new AttackSkill();
+			} else if (skillType.equalsIgnoreCase("RANGE")) {
+				sk = new RangeSkill();
+			} else if (skillType.equalsIgnoreCase("BUILD")) {
+				sk = new BuildSkill();
+			} else if (skillType.equalsIgnoreCase("HEAL")) {
+				sk = new HealSkill();
+			}
+			skills.put(skillType, sk);
 		}
 
-		return null;
+		return sk;
 	}
 
 }
